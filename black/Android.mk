LineageOS/android_packages_overlays_Lineage

overlays: add black style
Change-Id: I3414cfdb63929eae24b9942e095144d60b103cd1
Signed-off-by: Joey <joey@lineageos.org>

black/Android.mk
@@ -1,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageBlackTheme
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageBlackTheme
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)

LMODroid/platform_packages_overlays_LMODroid

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

black/Android.mk
@@ -1,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LMODroidBlackTheme
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LMODroidBlackTheme
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)