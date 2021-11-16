LineageOS/android_packages_overlays_Lineage

Introduce {Lato,Rubik} font overlay packages
Change-Id: I3554dc6269955d6922962ef2a773d1ed55b750a2

fonts/Lato/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageLatoFont
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageLatoFont
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)

fonts/Rubik/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageRubikFont
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageRubikFont
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)

LMODroid/platform_packages_overlays_LMODroid

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

fonts/Lato/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LMODroidLatoFont
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LMODroidLatoFont
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)

fonts/Rubik/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LMODroidRubikFont
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LMODroidRubikFont
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)